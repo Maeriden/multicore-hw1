2eb215a9e96843e8ae8cac83400a2622b984514a
Daniele Bondi <bondi.1701014@studenti.uniroma1.it>
2017-12-30 18:30:17 +0100

    Modificati gli script di run

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ setlocal
 
 set classpath=lib\argparse4j-0.8.1.jar;bin\mchw1.jar
 set mainclass=mchw1.Main
-java -cp %classpath% -XX:MaxHeapSize=1G %mainclass% %*
+java -cp %classpath% -XX:MaxHeapSize=4G %mainclass% %*
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 classpath='lib/argparse4j-0.8.1.jar:bin/mchw1.jar'
 mainclass='mchw1.Main'
-java -cp ${classpath} -XX:MaxHeapSize=2G ${mainclass} "$@"
+java -cp ${classpath} -XX:MaxHeapSize=4G ${mainclass} "$@"
